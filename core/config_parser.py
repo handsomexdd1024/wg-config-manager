57d96c788871413dc0dbc224179f88bfe83f3bd7
Billy Yang <handsomexdd1024@gmail.com>
2023-05-11 15:30:33 +0800

    work

--- core/config_parser.py
+++ core/config_parser.py
@@ -4,3 +4,10 @@ Generate configuration files from given wireguard network object, and vice versa
 """
 
 import yaml
+
+from core.wireguard_network import WireGuardNetwork, WireGuardNode
+
+
+class ConfigParser:
+    # TODO
+    pass
