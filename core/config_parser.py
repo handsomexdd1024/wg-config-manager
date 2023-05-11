7e0ea55dcc44801ad064d41568d62ba6dd17a9f6
Billy Yang <handsomexdd1024@gmail.com>
2023-05-11 14:52:04 +0800

    Add config_parser.py; add content to user.py

---
+++ core/config_parser.py
@@ -0,0 +1,6 @@
+"""
+Configuration file parser for core module.
+Generate configuration files from given wireguard network object, and vice versa.
+"""
+
+import yaml
---
+++ core/user.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2023 Billy Yang.
+# This software is licensed under the GNU AGPLv3 license.
+
+"""
+User module for WireGuard configuration management system.
+"""
+
+from abc import ABC, abstractmethod
+import bcrypt as hash
+import uuid
+
+
+class User(ABC):
+    """
+    This class represents a user.
+    """
+
+    def __init__(
+            self,
+            identifier: uuid.UUID,
+            name: str,
+            hashed_password: bytes,
+            salt: bytes
+    ):
+        self.name = name
+        self.uuid = identifier
+        self.hashed_password = hashed_password
+        self.salt = salt
+
+    def authenticate(self, plaintext: str) -> bool:
+        return hash.checkpw(plaintext.encode("bytes"), self.hashed_password)
