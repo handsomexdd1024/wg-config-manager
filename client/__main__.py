ad73f5ddaa021d1b13910e1495affc9e4023b8cc
JaysonLuo <2628498451@qq.com>
2023-05-23 21:50:25 +0800

    Create mainwindow and complete API

--- client/__main__.py
+++ client/__main__.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QFont, QIcon, QPalette, QColor, QPixmap, QMovie
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QSplitter, QDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QSplitter, QDialog, QScrollArea
 from qt_material import apply_stylesheet
 
 
@@ -24,6 +24,7 @@ class LoginWindow(QWidget):
         super().__init__()
 
         # 创建一个水平布局
+        self.main_window = None
         self.h_layout = QHBoxLayout(self)
 
         # 创建一个分割窗口，左右各占一半
@@ -87,7 +88,7 @@ class LoginWindow(QWidget):
         # 设置窗口属性
         self.setGeometry(745, 445, 800, 500)
 
-        self.setWindowIcon(QIcon('../resource/vscode.jfif'))
+        self.setWindowIcon(QIcon('../resource/bupt1.png'))
         self.setWindowTitle('Fast WireGuard Network Manager')
         self.login_button.clicked.connect(self.login)
 
@@ -103,9 +104,124 @@ class LoginWindow(QWidget):
 
         # TODO: 在此处编写验证登录信息的代码
 
+        # 登陆成功后跳转到主界面
+        self.hide()
+        self.main_window = MainWindow()
+        self.main_window.show()
         print('账号：', username)
         print('密码：', password)
 
+    # 主界面
+
+
+from PyQt5.QtWidgets import QVBoxLayout, QScrollArea
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        # 窗口居中占满半个屏幕
+        self.resize(1680, 1150)
+        self.move(400, 250)
+        # 设置窗口标题
+        self.setWindowTitle('Fast WireGuard Network Manager')
+        # 设置窗口图标
+        # TODO: 换上一个正经的icon
+        # 整体使用水平布局
+        self.main_layout = QHBoxLayout(self)
+        # 创建一个主分割窗口分割左右两个部分
+        self.main_splitter = QSplitter(Qt.Horizontal, self)
+        self.main_layout.addWidget(self.main_splitter)
+
+        # 创建左侧的文件栏，用于列出文件
+        self.file_widget = QWidget(self.main_splitter)
+        self.file_layout = QVBoxLayout(self.file_widget)
+        self.file_widget.setStyleSheet('background-color: #3c3f41')
+
+        # 创建一个垂直布局用于容纳文件栏和滚动条
+        self.file_layout_with_scroll = QVBoxLayout(self.file_widget)
+        self.file_layout_with_scroll.setContentsMargins(0, 0, 0, 0)
+        self.file_layout_with_scroll.setSpacing(0)
+
+        # 使用滚动区域包装文件栏部件
+        self.file_scroll_area = QScrollArea()
+        self.file_scroll_area.setWidgetResizable(True)
+        self.file_scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.file_scroll_area.setWidget(self.file_widget)
+
+        self.main_splitter.addWidget(self.file_scroll_area)
+
+        # 创建一个右侧的分割窗口用于上下分割
+        self.right_splitter = QSplitter(Qt.Vertical, self.main_splitter)
+
+        # 创建右侧上方的主窗口
+        self.top_widget = QWidget(self.right_splitter)
+        self.top_widget.setStyleSheet('background-color: #3c3f41')
+
+        # 底部创建一个水平分割栏，用于设置三个按钮，分别为更新、添加、删除
+        self.bottom_splitter = QSplitter(Qt.Horizontal, self.right_splitter)
+        self.bottom_text_widget = QWidget(self.bottom_splitter)
+        self.bottom_button_widget = QWidget(self.bottom_splitter)
+        self.bottom_splitter.setHandleWidth(6)
+        self.bottom_text_widget.setStyleSheet('background-color: #3c3f41')
+        self.bottom_button_widget.setStyleSheet('background-color: #3c3f41')
+
+        # 调整伸缩性，使得右上方的主窗口可以伸缩
+        self.right_splitter.setStretchFactor(0, 3)
+        self.right_splitter.setStretchFactor(1, 1)
+        self.main_splitter.setStretchFactor(1, 1)
+
+        # 设置左右splitter初始宽度比例1：3
+        self.right_splitter.setSizes([3, 1])
+        self.main_splitter.setSizes([self.width() // 4, self.width() * 3 // 4])
+        self.right_splitter.setSizes([self.width() * 3 // 4, self.width() // 4])
+        self.bottom_splitter.setSizes([self.width() * 3 // 4, self.width() // 4])
+
+        # 设置边框宽度
+        self.right_splitter.setHandleWidth(5)
+
+        # 创建底部按钮,用于新建配置文件
+        self.file_button = QPushButton("New config", self.file_widget)
+        self.file_layout.addStretch()
+        self.file_layout.addWidget(self.file_button, alignment=Qt.AlignCenter)
+        self.file_button.setStyleSheet('color: white; font-size: 20px; font-family: "Microsoft YaHei"')
+        self.file_button.clicked.connect(self.new_config)
+
+    def get_network_info(self):
+        # TODO: 在此处编写获取该用户网络信息的代码
+        pass
+
+    def show_config(self):
+        # TODO: 在此处编写显示获取的配置文件的代码
+        pass
+
+    def new_config(self):
+        # TODO: 在此处编写新建配置文件的代码
+        pass
+
+    def show_graph(self):
+        # TODO: 在此处编写显示获取的图表的代码
+        pass
+
+    def show_node(self):
+        # TODO: 在此处编写显示点击的的节点信息的代码
+        pass
+    
+    def update_wireguard_node(self):
+        # TODO: 在此处编写修改节点信息的代码
+        pass
+
+    def create_wireguard_node(self):
+        # TODO: 在此处编写新建节点信息的代码
+        pass
+    
+    def delete_wireguard_node(self):
+        # TODO: 在此处编写删除节点信息的代码
+        pass
+
+    def generate_config(self):
+        # TODO: 在此处编写生成配置文件的代码
+        pass
 
 class ChildDialogUi(QDialog):
     def __init__(self, text):
