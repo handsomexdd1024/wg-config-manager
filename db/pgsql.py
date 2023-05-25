734bc272bb88e538c87f1af91135b726281b25f3
kathrine <1795206343@qq.com>
2023-05-25 17:27:44 +0800

    db完善

--- db/pgsql.py
+++ db/pgsql.py
@@ -11,7 +11,108 @@ conn = psycopg.connect(
 )
 # 创建游标对象
 cur = conn.cursor()
+# 创建数据库
+def create_database():
+    try:
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE wireguard")
+        cursor.close()
+        conn.commit()
+        print("Database created successfully!")
+    except (Exception, psycopg2.Error) as error:
+        print("Error creating database:", error)
 
+# 创建模式
+def create_schema():
+    try:
+        cursor = conn.cursor()
+        cursor.execute("CREATE SCHEMA wireguard")
+        cursor.close()
+        conn.commit()
+        print("Schema created successfully!")
+    except (Exception, psycopg2.Error) as error:
+        print("Error creating schema:", error)
+
+# 创建表格
+def create_tables():
+    try:
+        cursor = conn.cursor()
+        # 创建schema WireguardNode下的表格 NodeType
+        cursor.execute("""
+            CREATE TABLE wireguard.NodeType (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255)
+            )
+        """)
+
+        # 创建schema WireguardNode下的表格 EndpointType
+        cursor.execute("""
+            CREATE TABLE wireguard.EndpointType (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255)
+            )
+        """)
+
+        # 创建schema WireguardNode下的表格 self
+        cursor.execute("""
+            CREATE TABLE wireguard.self (
+                identifier UUID PRIMARY KEY,
+                owner UUID,
+                name VARCHAR(255),
+                address_list TEXT,
+                admin_approval BOOLEAN,
+                node_type VARCHAR(255),
+                private_key TEXT,
+                public_key TEXT,
+                endpoint TEXT
+            )
+        """)
+
+        # 创建schema WireguardObject下的表格 ObjectType
+        cursor.execute("""
+            CREATE TABLE wireguard.ObjectType (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255)
+            )
+        """)
+
+        # 创建schema WireguardObject下的表格 self
+        cursor.execute("""
+            CREATE TABLE wireguard.self (
+                identifier UUID PRIMARY KEY,
+                object_type INTEGER
+            )
+        """)
+
+        # 创建schema WireguardConnection下的表格 self
+        cursor.execute("""
+            CREATE TABLE wireguard.self (
+                identifier TEXT PRIMARY KEY,
+                peers TEXT,
+                preshared_key TEXT
+            )
+        """)
+
+        # 创建schema WireguardNetwork下的表格 self
+        cursor.execute("""
+            CREATE TABLE wireguard.self (
+                identifier TEXT PRIMARY KEY,
+                name VARCHAR(255),
+                node_uuid_list TEXT,
+                connection_uuid_list TEXT
+            )
+        """)
+
+        cursor.close()
+        conn.commit()
+        print("Tables created successfully!")
+    except (Exception, psycopg2.Error) as error:
+        print("Error creating tables:", error)
+
+# 创建数据库、模式和表格
+create_database()
+create_schema()
+create_tables()
 #提交和回滚事务
 
 try:
