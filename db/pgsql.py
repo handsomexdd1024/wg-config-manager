cac45a168bada33e4a239469c6b812e368f53aa3
kathrine <1795206343@qq.com>
2023-06-03 13:56:58 +0800

    db完善

--- db/pgsql.py
+++ db/pgsql.py
@@ -1,6 +1,7 @@
 import psycopg2
 import psycopg2.extras
 import uuid
+import pickle
 from enum import Enum
 
 # 枚举类型定义
@@ -14,17 +15,16 @@ class EndpointType(Enum):
     IPV6 = 1
     DOMAIN = 2
 
-# 连接到数据库
+# 连接到数据库hxy
 conn = psycopg2.connect(
-host="localhost",
-port="7777",
-database="postgres",
-user="postgres",
-password="hxy20030620"
+    host="localhost",
+    port="7777",
+    database="postgres",
+    user="postgres",
+    password="hxy20030620"
 )
-cur = conn.cursor()
 
-# 创建表格和类型转换
+cur = conn.cursor()
 
 # NodeType和EndpointType转换函数
 def convert_node_type(node_type):
@@ -53,12 +53,36 @@ def convert_list_to_str(lst):
 def convert_str_to_list(lst_str):
     return lst_str.split(',')
 
+# PEER（Python的序列化格式）和字符串之间的转换
+def convert_peer_to_str(peer):
+    return pickle.dumps(peer)
+
+def convert_str_to_peer(peer_str):
+    return pickle.loads(peer_str)
+
+# UUID和字符串之间的转换
+def convert_uuid_to_str(uuid_obj):
+    return str(uuid_obj)
+
+def convert_str_to_uuid(uuid_str):
+    return uuid.UUID(uuid_str)
+
+# 字节串（bytes）和字符串之间的转换
+def convert_bytes_to_str(bytes_obj):
+    return bytes_obj.decode('utf-8')
+
+def convert_str_to_bytes(str_obj):
+    return str_obj.encode('utf-8')
+
 # 注册类型转换函数
-psycopg2.extras.register_uuid()
-psycopg2.extensions.register_adapter(NodeType, convert_node_type)
-psycopg2.extensions.register_adapter(EndpointType, convert_endpoint_type)
+psycopg2.extensions.register_adapter(NodeType, lambda x: x.value)
+psycopg2.extensions.register_adapter(EndpointType, lambda x: x.value)
 psycopg2.extensions.register_adapter(uuid.UUID, convert_uuid_to_str)
 psycopg2.extensions.register_adapter(list, convert_list_to_str)
+psycopg2.extensions.register_adapter(bytes, convert_bytes_to_str)
+psycopg2.extensions.register_adapter(str, convert_str_to_bytes)
+psycopg2.extensions.register_adapter(object, convert_peer_to_str)
+psycopg2.extensions.register_adapter(str, convert_str_to_peer)
 
 # 逆向类型转换函数
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
@@ -71,33 +95,31 @@ psycopg2.extensions.register_adapter(str, convert_str_to_list)
 psycopg2.extras.register_uuid()
 
 # 创建表格
+
 cur.execute("""
-    CREATE TABLE NodeType (
+    CREATE TABLE IF NOT EXISTS NodeType (
         id SERIAL PRIMARY KEY,
         name VARCHAR(50) NOT NULL
     )
 """)
-
 cur.execute("""
-    CREATE TABLE EndpointType (
+    CREATE TABLE IF NOT EXISTS ObjectType (
         id SERIAL PRIMARY KEY,
         name VARCHAR(50) NOT NULL
     )
 """)
-
 cur.execute("""
-    CREATE TABLE ObjectType (
+    CREATE TABLE IF NOT EXISTS EndpointType (
         id SERIAL PRIMARY KEY,
         name VARCHAR(50) NOT NULL
     )
 """)
-
 cur.execute("""
-    CREATE TABLE WireguardNode (
+    CREATE TABLE IF NOT EXISTS WireguardNode (
         identifier UUID PRIMARY KEY,
         owner UUID NOT NULL,
         name VARCHAR(50),
-        oaddress_list VARCHAR,
+        address_list VARCHAR,
         admin_approval BOOLEAN,
         node_type INTEGER REFERENCES NodeType(id),
         private_key VARCHAR,
@@ -107,22 +129,22 @@ cur.execute("""
 """)
 
 cur.execute("""
-    CREATE TABLE WireguardObject (
+    CREATE TABLE IF NOT EXISTS WireguardObject (
         identifier UUID PRIMARY KEY,
         object_type INTEGER REFERENCES ObjectType(id)
     )
 """)
 
 cur.execute("""
-    CREATE TABLE WireguardConnection (
+    CREATE TABLE IF NOT EXISTS WireguardConnection (
         identifier UUID PRIMARY KEY,
-        opeers VARCHAR,
+        peers VARCHAR,
         preshared_key VARCHAR
     )
 """)
 
 cur.execute("""
-    CREATE TABLE WireguardNetwork (
+    CREATE TABLE IF NOT EXISTS WireguardNetwork (
         identifier UUID PRIMARY KEY,
         name VARCHAR(50),
         node_uuid_list VARCHAR,
@@ -131,7 +153,7 @@ cur.execute("""
 """)
 
 cur.execute("""
-    CREATE TABLE user_self (
+    CREATE TABLE IF NOT EXISTS user_self (
         identifier UUID PRIMARY KEY,
         name VARCHAR(50),
         hashed_password VARCHAR,
@@ -140,7 +162,7 @@ cur.execute("""
 """)
 
 cur.execute("""
-    CREATE TABLE WireguardConfig (
+    CREATE TABLE IF NOT EXISTS WireguardConfig (
         identifier UUID PRIMARY KEY,
         owner UUID,
         name VARCHAR(50),
@@ -150,7 +172,7 @@ cur.execute("""
 """)
 
 cur.execute("""
-    CREATE TABLE StandardResponse (
+    CREATE TABLE IF NOT EXISTS StandardResponse (
         code INTEGER,
         message VARCHAR,
         content VARCHAR
@@ -158,14 +180,14 @@ cur.execute("""
 """)
 
 cur.execute("""
-    CREATE TABLE NetworkModification_Action (
+    CREATE TABLE IF NOT EXISTS NetworkModification_Action (
         id SERIAL PRIMARY KEY,
         name VARCHAR(50) NOT NULL
     )
 """)
 
 cur.execute("""
-    CREATE TABLE NetworkModification (
+    CREATE TABLE IF NOT EXISTS NetworkModification (
         action INTEGER REFERENCES NetworkModification_Action(id),
         content VARCHAR
     )
@@ -193,7 +215,6 @@ cur.execute("""
 cur.execute("""
     DELETE FROM NodeType WHERE id = 1
 """)
-
 
 # 提交更改并关闭连接
 conn.commit()
