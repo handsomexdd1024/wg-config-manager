1657db138745fb612cd1bccc745b60f76f34e8f5
Billy Yang <me@venti.love>
2023-06-09 23:52:33 +0800

    add auto construction

--- client/utils.py
+++ client/utils.py
@@ -1,6 +1,9 @@
+import secrets
+
 from object_loader import ObjectLoader
 from core import *
-from uuid import UUID
+from uuid import UUID, uuid4
+from enum import Enum
 
 import wireguard_tools as wgtools
 
@@ -55,6 +58,91 @@ def gen_config(
 
     return conf
 
+
+class ConstructType(Enum):
+    """
+    This enum is used to specify the type of auto_construct.
+    """
+    P2P = 0
+    STAR = 1
+
+
+def _auto_construct_p2p(
+        node_id_list: list[UUID],
+        loader: ObjectLoader
+) -> list[wgobject.WireguardConnection]:
+    nodes = []
+    for nid in node_id_list:
+        node = loader[nid]
+        if node.endpoint is None:
+            raise ValueError("Node has no endpoint, cannot construct full-mesh network.")
+        nodes.append(node)
+    connections = []
+    # for each two nodes, construct a connection with a preshared key
+    for i in range(len(nodes)):
+        for j in range(i + 1, len(nodes)):
+            connection = wgobject.WireguardConnection(
+                identifier=UUID(bytes=uuid4().bytes),
+                preshared_key=wgobject.WireguardKey(
+                    keydata=secrets.token_bytes(32)
+                ),
+                peers=[nodes[i].identifier, nodes[j].identifier]
+            )
+            connections.append(connection)
+    return connections
+
+
+def _auto_construct_star(
+        node_id_list: list[UUID],
+        loader: ObjectLoader
+) -> list[wgobject.WireguardConnection]:
+    clients = []
+    router = None
+    connections = []
+    for nid in node_id_list:
+        node = loader[nid]
+        if node.endpoint is not None:
+            if router is not None:
+                raise ValueError("More than one node has an endpoint, cannot construct star network.")
+            router = node
+        else:
+            clients.append(node)
+    if router is None:
+        raise ValueError("No node has an endpoint, cannot construct star network.")
+    for client in clients:
+        connection = wgobject.WireguardConnection(
+            identifier=UUID(bytes=uuid4().bytes),
+            preshared_key=wgobject.WireguardKey(
+                keydata=secrets.token_bytes(32)
+            ),
+            peers=[router.identifier, client.identifier]
+        )
+        connections.append(connection)
+    return connections
+
+
+def auto_construct(
+        node_id_list: [UUID],
+        loader: ObjectLoader,
+        construct_type: ConstructType
+) -> list[wgobject.WireguardConnection] :
+    # security checks
+    for nid in node_id_list:
+        if nid not in loader:
+            raise ValueError("Node not found in cache.")
+        node = loader[nid]
+        if not isinstance(node, wgconfig.WireguardNode):
+            raise ValueError("Object is not a WireguardNode.")
+
+    # construct
+    if construct_type == ConstructType.P2P:
+        return _auto_construct_p2p(node_id_list, loader)
+    elif construct_type == ConstructType.STAR:
+        return _auto_construct_star(node_id_list, loader)
+    else:
+        raise ValueError("Invalid construct_type.")
+
+
 def validity_check(
         loader: ObjectLoader,
         network: wgconfig.WireguardNetwork
@@ -78,4 +166,3 @@ def validity_check(
     for cid in c:
         if cid not in network.connection_uuid_list:
             return False, "Connection not found in network."
-
